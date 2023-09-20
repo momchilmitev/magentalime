06226929bb21df0af889be816d2891819c02eaed
momchilmitev <momchilmitev87@gmail.com>
2023-09-20 07:31:35 +0200

    cleaning the files and starting the new project

--- views/addjoke.html.php
+++
@@ -1,6 +0,0 @@
-<form method="post" action="addjoke.php">
-    <label for="joketext">First Name:</label>
-    <textarea type="joketext" name="joketext" id="joketext" rows="3" cols="40"></textarea>
-
-    <input type="submit" name="submit" value="add">
-</form>
--- views/home.html.php
+++
@@ -1,3 +0,0 @@
-<h2>The best online jokes</h2>
-
-<p>Welcome to the online jokes oasis</p>
